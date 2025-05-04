e45073809c767182cd390449e07c5729ac804559
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-05-04 14:58:18 +0100

    Debug - change CSS to css

--- codestar/settings.py
+++ codestar/settings.py
@@ -148,6 +148,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Enable WhiteNoise to serve static files in production
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
