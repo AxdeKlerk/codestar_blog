09b522e3435874f51abf0bfbe7038a5369ed43bd
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-05-04 15:16:25 +0100

    Use CompressedStaticFilesStorage instead of Manifest to avoid missing‐file errors

--- codestar/settings.py
+++ codestar/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # URL path for serving static files
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
